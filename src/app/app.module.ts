068c0d68fe360cbd64d59d34498f61c8bf9bf274
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2019-08-12 19:14:58 +0200

    Home, About Me, Portfolio, Contact and Navbar components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutMeComponent,
+    PortfolioComponent,
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
