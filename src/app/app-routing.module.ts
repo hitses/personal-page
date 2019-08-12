5aba96118ce3d60b60105a090cc6ba4f4cf88d8f
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2019-08-12 19:27:28 +0200

    Routing configured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'about-me', component: AboutMeComponent},
+  {path: 'portfolio', component: PortfolioComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '**', redirectTo: '/home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
